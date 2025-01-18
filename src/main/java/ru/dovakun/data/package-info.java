fa9e06ee1631fc10bd19e2ab9847f14410b2f803
start.vaadin.com <no-reply@vaadin.com>
2025-01-18 22:20:39 +0000

    Generated project

---
+++ src/main/java/ru/dovakun/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package ru.dovakun.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/ru/dovakun/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package ru.dovakun.services;
+
+import org.springframework.lang.NonNullApi;
